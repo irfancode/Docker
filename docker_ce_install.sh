2b0efbc41639ddd94b0ffb7a02155aba5eb9bad0
Irfan Shaikh <irfan.shaikh@me.com>
2017-10-06 14:59:55 +0800

    Updated.

--- docker_ce_install.sh
+++ docker_ce_install.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 date "+DAY : %A%nDATE: %m/%d/%y%nTIME: %H:%M:%S"
@@ -24,31 +23,3 @@ sudo apt-get update
 sudo apt-get install docker-ce
 
 sudo docker run hello-world
-=======
-#!/bin/sh
-## Usage: docker_ce_install.sh [options] ARG1
-##
-## Options:
-##   -h, --help    Display this message.
-##   -n            Dry-run; only show what would be done.
-##
-
-usage() {
-  [ "$*" ] && echo "$0: $*"
-  sed -n '/^##/,/^$/s/^## \{0,1\}//p' "$0"
-  exit 2
-} 2>/dev/null
-
-main() {
-  while [ $# -gt 0 ]; do
-    case $1 in
-    (-n) DRY_RUN=1;;
-    (-h|--help) usage 2>&1;;
-    (--) shift; break;;
-    (-*) usage "$1: unknown option";;
-    (*) break;;
-    esac
-  done
-  : do echo 'stuff'.
-}
->>>>>>> 8e197e4706848d10dd3e5b768e0e06d4ce025fcb
